23c7f9b338281dc727a24fb91778eaf26181c10e
Matias Salomon <matias.salomon@clt.com.py>
2023-12-28 11:01:48 -0300

    Colors

---
+++ lib/colors/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color backgroundColor = Color(0xff000000);
+const Color lightBlue = Color(0xff0f8ee0);
+const Color red = Color(0xffd82936);
+const Color white = Color(0xfff9f9f9);
+const Color whiteOpaque = Color(0xffd1cfd9);
