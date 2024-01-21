4b348da265030abcd0fcd70c1ea950854cd447a0
MatiasSalomon01 <matisalomond@gmail.com>
2024-01-21 00:27:38 -0300

    cambios varios

--- lib/providers/reels_provider.dart
+++ lib/providers/reels_provider.dart
@@ -8,6 +8,7 @@ class ReelsProvider extends ChangeNotifier {
   Map<int, VideoPlayerController> mapVideos = {};
   Map<int, String> videosFromApi = {};
   int _currentIndex = 0;
+  int _page = 0;
 
   int get currentIndex => _currentIndex;
 
@@ -16,16 +17,6 @@ class ReelsProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  int _page = 0;
-
-  // var _list = [
-  //   'https://player.vimeo.com/external/226685105.hd.mp4?s=b6a194faf216cac660ec198e72b4e939cd74dee3&profile_id=174&oauth2_token_id=57447761',
-  //   "https://player.vimeo.com/external/296210754.hd.mp4?s=08c03c14c04f15d65901f25b542eb2305090a3d7&profile_id=175&oauth2_token_id=57447761",
-  //   "https://player.vimeo.com/external/370467553.hd.mp4?s=ce49c8c6d8e28a89298ffb4c53a2e842bdb11546&profile_id=174&oauth2_token_id=57447761",
-  //   "https://player.vimeo.com/external/316506213.hd.mp4?s=1e169a0741d406dc6ff688d22a97cbeaf0261463&profile_id=175&oauth2_token_id=57447761",
-  //   'https://player.vimeo.com/external/424526465.hd.mp4?s=a7612c49100e87ab5470112277ce5c87a3222bb1&profile_id=175&oauth2_token_id=57447761',
-  // ];
-
   ReelsProvider() {
     getVideosFromApi();
   }
@@ -33,13 +24,10 @@ class ReelsProvider extends ChangeNotifier {
   Future<void> getVideosFromApi() async {
     int count = mapVideos.isEmpty ? 0 : videosFromApi.length;
 
-    // for (String url in _list) {
-    //   videosFromApi[count] = url;
-    //   count++;
-    // }
     _page += 1;
 
-    await getPexelsVideos(count: count);
+    bool finished = await getPexelsVideos(count: count);
+    if (!finished) return;
 
     await loadInitialVideos();
   }
@@ -126,11 +114,11 @@ class ReelsProvider extends ChangeNotifier {
   static const _headers = {
     'Authorization': 'c9P0gvk7SKSG0ypDARLPKSEHUEnNJJNAFvGwHnjoabJZLAzn3m9iEzT5'
   };
-  Future<void> getPexelsVideos({int count = 0}) async {
+
+  Future<bool> getPexelsVideos({int count = 0}) async {
     final url = Uri.https(
       _authority,
       '/videos/search',
-      // '/videos/search?query=nature&orientation=portrait&page=$page&per_page=5',
       {
         'query': 'nature',
         'orientation': 'portrait',
@@ -140,13 +128,12 @@ class ReelsProvider extends ChangeNotifier {
     );
 
     var response = await http.get(url, headers: _headers);
-    if (response.statusCode != 200) return;
 
-    // response.headers['X-Ratelimit-Remaining']
+    if (response.statusCode != 200) return false;
+
     var decodedBody = json.decode(response.body) as Map<String, dynamic>;
     var pexelsReponse = PexelsResponse.fromJson(decodedBody);
 
-    // int i = 0;
     for (var pexel in pexelsReponse.videos) {
       var videoFiles = pexel.videoFiles
           .where((e) => e.height <= 900 && e.width <= 540)
@@ -157,6 +144,6 @@ class ReelsProvider extends ChangeNotifier {
       count++;
     }
     notifyListeners();
-    // return pexelsReponse;
+    return true;
   }
 }
